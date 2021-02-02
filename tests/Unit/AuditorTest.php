d685151b74c6e7bcdb6bc974c865380843a42c7c
Leon Stafford <me@ljs.dev>
2021-02-02 18:19:25 +1000

    wip adjusting phpcs

--- tests/Unit/AuditorTest.php
+++ tests/Unit/AuditorTest.php
@@ -13,23 +13,28 @@ declare(strict_types=1);
 
 namespace WhatWouldViktorDo;
 
-use org\bovigo\vfs\vfsStream,
-    org\bovigo\vfs\vfsStreamDirectory;
+use org\bovigo\vfs\vfsStream;
 
-it('fails when no README.md in project', function () {
-    $this->project_dir = vfsStream::setup('project_dir');
+it(
+    'fails when no README.md in project',
+    function () {
+        $this->projectDir = vfsStream::setup('project_dir');
 
-    $auditor = new Auditor($this->project_dir->url());
+        $auditor = new Auditor($this->projectDir->url());
 
-    $this->assertFalse($auditor->hasReadme());
-});
+        $this->assertFalse($auditor->hasReadme());
+    }
+);
 
-it('passes when README.md is in project', function () {
-    $this->project_dir = vfsStream::setup('project_dir');
+it(
+    'passes when README.md is in project',
+    function () {
+        $this->projectDir = vfsStream::setup('project_dir');
 
-    file_put_contents($this->project_dir->url() . '/README.md', 'anything');
+        file_put_contents($this->projectDir->url() . '/README.md', 'anything');
 
-    $auditor = new Auditor(vfsStream::url('project_dir'));
+        $auditor = new Auditor(vfsStream::url('project_dir'));
 
-    $this->assertTrue($auditor->hasReadme());
-});
+        $this->assertTrue($auditor->hasReadme());
+    }
+);
