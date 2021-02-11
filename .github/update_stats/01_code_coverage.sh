dd5649450f792de8c12a48c8a69d1e91fd91a45a
Leon Stafford <me@ljs.dev>
2021-02-11 12:25:29 +1000

    force GH action rebuild

--- .github/update_stats/01_code_coverage.sh
+++ .github/update_stats/01_code_coverage.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 echo "repo's script"
-echo "r1 $NEW_COVERAGE" 
-echo "r2 ${{ env.NEW_COVERAGE }}" 
+echo "r1 $NEW_COVERAGE"
+echo "r2 ${{ env.NEW_COVERAGE }}"
+
 
 # replace code coverage % in README into temp file
 cat README.md | sed "/Coverage:/ c Coverage: $NEW_COVERAGE" | sed 's/\x1b\[[0-9;]*m//g' > NEWREADME.md
