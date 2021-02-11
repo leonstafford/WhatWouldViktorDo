d5ff4866f6c5005742875f48a4a1d7aef32deff3
Leon Stafford <me@ljs.dev>
2021-02-11 12:19:55 +1000

    try sourcable scripts

---
+++ .github/update_stats/01_code_coverage.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "repo's script"
+echo "r1 $NEW_COVERAGE" 
+echo "r2 ${{ env.NEW_COVERAGE }}" 
+
+# replace code coverage % in README into temp file
+cat README.md | sed "/Coverage:/ c Coverage: $NEW_COVERAGE" | sed 's/\x1b\[[0-9;]*m//g' > NEWREADME.md
+
+# overwrite README with tempfile
+mv NEWREADME.md README.md
