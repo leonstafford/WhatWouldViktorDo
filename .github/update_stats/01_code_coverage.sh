6379eb33301ab2a8c544ff354148d49dce8c438a
Leon Stafford <me@ljs.dev>
2021-02-11 13:15:50 +1000

    fix code cov replacement

--- .github/update_stats/01_code_coverage.sh
+++ .github/update_stats/01_code_coverage.sh
@@ -5,7 +5,7 @@ echo "a shell script within .github/update_stats/"
 echo "we have access to the variable set within our GH action yml: $NEW_COVERAGE"
 
 # replace code coverage % in README into temp file
-cat README.md | sed "/Coverage:/ c Coverage: $NEW_COVERAGE" | sed 's/\x1b\[[0-9;]*m//g' > NEWREADME.md
+sed -i '' 's/Coverage:.*/Coverage: '"$NEW_COVERAGE"'/;s/\x1b\[[0-9;]*m//g' README.md
 
 # TODO: optionally check for changes, annotate with "increased/decreased" compared to last build, etc
 # or report when no project stats have changed, maybe exit code form this can determine if subsequent commit action takes place?
