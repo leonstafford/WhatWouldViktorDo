a754779aa1eed5bfbf0675767c98a73766c400a1
Leon Stafford <me@ljs.dev>
2021-02-11 12:41:22 +1000

    tidy up main GH action yml;debug script

--- .github/update_stats/01_code_coverage.sh
+++ .github/update_stats/01_code_coverage.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo "repo's script"
-
-echo "r1 $NEW_COVERAGE"
-
-# echo "r2 ${{ env.NEW_COVERAGE }}"
-
+echo "Debug:"
+echo "a shell script within .github/update_stats/"
+echo "we have access to the variable set within our GH action yml: $NEW_COVERAGE"
 
 # replace code coverage % in README into temp file
-cat README.md | sed "/Coverage:/ c Coverage: $NEW_COVERAGE is working" | sed 's/\x1b\[[0-9;]*m//g' > NEWREADME.md
+cat README.md | sed "/Coverage:/ c Coverage: $NEW_COVERAGE" | sed 's/\x1b\[[0-9;]*m//g' > NEWREADME.md
+
+# TODO: optionally check for changes, annotate with "increased/decreased" compared to last build, etc
+# or report when no project stats have changed, maybe exit code form this can determine if subsequent commit action takes place?
 
 # overwrite README with tempfile
 mv NEWREADME.md README.md
